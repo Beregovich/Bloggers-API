56b44a552636ea777ef3cd93f7a1adfee9a54efe
Beregovich <beregovich@mail.ru>
2022-04-16 10:28:12 +0300

    find  bug where autotesting

--- src/repositories/posts-repository.ts
+++ src/repositories/posts-repository.ts
@@ -12,7 +12,7 @@ export type PostToPushType = {
     title: string | null;
     shortDescription: string | null;
     content: string | null;
-    bloggerId: number;
+    blogId: number;
 }
 
 export const postsRepository = {
@@ -22,7 +22,7 @@ export const postsRepository = {
         await posts.forEach(p => {
             postsWithNames.push({
                 ...p,
-                bloggerName: bloggers.find(b => b.id === p.bloggerId)?.name
+                bloggerName: bloggers.find(b => b.id === p.blogId)?.name
             })
         })
         return postsWithNames
